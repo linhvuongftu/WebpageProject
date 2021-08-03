e5dfdb22f7346462fe10ee8d5cfb4545bb83f0b2
john <linhvuongftu@gmail.com>
2021-08-03 10:38:02 -0400

    Csharp HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Revature!");
+            Console.WriteLine("The curent time is " + DateTime.Now);
+
+            Console.WriteLine("Enter something: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("You Entered " + input);
+
+            if (input == ""){
+                Console.WriteLine("You Entered " + input);
+            }
+
+        }
+    }
+}
